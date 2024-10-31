3906c2817ff4660b389f0ce86f7dfc12bb176fa6
lnkkerst <lnkkerst04@gmail.com>
2024-10-31 22:51:51 +0800

    feat: hit by GET

--- src/app/[...key]/route.ts
+++ src/app/[...key]/route.ts
@@ -1,5 +1,6 @@
 import { ensureKey, kvPrefix } from "@/utils";
 import { kv } from "@vercel/kv";
+import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
@@ -7,9 +8,17 @@ type Params = {
   key: string[];
 };
 
-export async function GET(_: Request, context: { params: Params; }) {
+export async function GET(request: NextRequest, context: { params: Params; }) {
   const key = context.params.key.join("/");
-  const value = await ensureKey(key);
+  const hit = request.nextUrl.searchParams.get("hit");
+  let value = 0;
+  if (hit !== null) {
+    const namespace = context.params.key[0];
+    await kv.incr(`${kvPrefix}${namespace}`);
+    value = await kv.incr(`${kvPrefix}${key}`);
+  } else {
+    value = await ensureKey(key);
+  }
 
   return Response.json({ value });
 }
